0a45852850170bbf3d47c7b328fcc0766aa9ea6a
mozieblo <m.ozieblo@wp.pl>
2019-08-01 17:38:46 +0200

    update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// First solution
 var appRoot = document.getElementById('app');
 
 var show = {
@@ -36,4 +37,37 @@ var renderApp = function renderApp() {
     ReactDOM.render(template, appRoot);
 };
 
-renderApp();
+//renderApp();
+
+//Second better! solutions
+var visibility = false;
+
+var visibilityToggle = function visibilityToggle() {
+    visibility = !visibility;
+    renderApp2();
+};
+
+var renderApp2 = function renderApp2() {
+    var template = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Visibility Toggle'
+        ),
+        React.createElement(
+            'button',
+            { onClick: visibilityToggle },
+            visibility ? 'Hide details' : 'Show details'
+        ),
+        React.createElement(
+            'p',
+            null,
+            visibility && 'Hey! These are some details you can now see!'
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp2();
--- src/playground/built-it-visible.js
+++ src/playground/built-it-visible.js
@@ -1,3 +1,4 @@
+// First solution
 const appRoot = document.getElementById('app');
 
 const show = {
@@ -22,4 +23,25 @@ const renderApp = () => {
     ReactDOM.render(template, appRoot);
 };
 
-renderApp();
+//renderApp();
+
+//Second better! solutions
+let visibility = false;
+
+const visibilityToggle = () => {
+    visibility = !visibility;
+    renderApp2();
+};
+
+const renderApp2 = () => {
+    const template = (
+        <div>
+            <h1>Visibility Toggle</h1>
+            <button onClick={visibilityToggle}>{visibility ? 'Hide details' : 'Show details'}</button>
+            <p>{visibility && 'Hey! These are some details you can now see!' }</p>
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+};
+
+renderApp2();
