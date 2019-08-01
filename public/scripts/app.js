8901818c6365b572b922bf2f666c94601d3847ba
mozieblo <m.ozieblo@wp.pl>
2019-08-01 14:09:52 +0200

    update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,46 +2,88 @@
 
 console.log('App.js is running!');
 
+var appRoot = document.getElementById('app');
+
 var app = {
     title: 'Indecision Start App',
     subtitle: 'Choose your destiny',
-    options: ['one', 'two']
+    options: []
 };
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        ' Subtitle : ',
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
+
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
+};
+
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    renderApp();
+};
+
+var onDecisionMake = function onDecisionMake() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    alert(option);
+};
+
+var renderApp = function renderApp() {
+    var template = React.createElement(
+        'div',
         null,
         React.createElement(
-            'li',
+            'h1',
             null,
-            'New item'
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            ' Subtitle : ',
+            app.subtitle
         ),
         React.createElement(
-            'li',
+            'p',
             null,
-            'Second item'
+            app.options.length > 0 ? 'Here are your options' : 'No options'
+        ),
+        React.createElement(
+            'button',
+            { disabled: app.options.length === 0, onClick: onDecisionMake },
+            'What should I do?'
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove All'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            app.options.map(function (item) {
+                return React.createElement(
+                    'li',
+                    { key: item },
+                    item
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
         )
-    )
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+    );
+    ReactDOM.render(template, appRoot);
+};
+renderApp();
